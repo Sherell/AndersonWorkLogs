80d843c2e9c1501f2a9ed16e631ee1ab4be16c2a
sbteruel21 <sbteruel21@gmail.com>
2017-12-19 08:55:19 +0800

    sheyganda

---
+++ AndersonWorkLogsWeb/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using AccountsFunction;
+using AccountsWebAuthentication.Helper;
+using AndersonWorkLogsWeb.Controllers;
+using System.Web.Mvc;
+
+namespace AccountsWeb.Controllers
+{
+    public class UserController : BaseController
+    {
+        private IFUser _iFUser;
+        public UserController(IFUser iFUser)
+        {
+            _iFUser = iFUser;
+        }
+
+        #region Read
+        [HttpPost]
+        public JsonResult Read()
+        {
+            return Json(_iFUser.Read());
+        }
+        #endregion
+    }
+}
--- AndersonWorkLogsWeb/Scripts/Angular/Controller/UserController.js
+++ AndersonWorkLogsWeb/Scripts/Angular/Controller/UserController.js
@@ -3,55 +3,53 @@
 
     angular
         .module('App')
-        .controller('AttendanceController', AttendanceController);
+        .controller('UserController', UserController);
 
-    AttendanceController.$inject = ['$filter', '$window', 'AttendanceService','UserService'];
+    UserController.$inject = ['$filter', '$window', 'EmployeeService', 'UserService'];
 
-    function AttendanceController($filter, $window, AttendanceService, UserService) {
+    function UserController($filter, $window, EmployeeService, UserService) {
         var vm = this;
 
-         
-        vm.AttendanceId;
-
-        vm.Attendances = [];
+        vm.Employees = [];
         vm.Users = [];
 
         vm.GoToUpdatePage = GoToUpdatePage;
         vm.Initialise = Initialise;
 
+        vm.UpdateEmployee = UpdateEmployee;
+
         vm.Delete = Delete;
 
-        function GoToUpdatePage(attendanceId) {
-            $window.location.href = '../Attendance/Update/' + attendanceId;
+        function GoToUpdatePage(userId) {
+            $window.location.href = '../User/Update/' + userId;
         }
 
         function Initialise() {
             Read();
+            ReadEmployees();
+        }
+
+        function ReadEmployees() {
+            EmployeeService.Read()
+                .then(function (response) {
+                    vm.Employees = response.data;
+                })
+                .catch(function (data, status) {
+                    new PNotify({
+                        title: status,
+                        text: data,
+                        type: 'error',
+                        hide: true,
+                        addclass: "stack-bottomright"
+                    });
+
+                });
         }
 
         function Read() {
-            AttendanceService.Read()
-                .then(function (response) {
-                    vm.Attendances = response.data;
-                    ReadUsers();
-                })
-                .catch(function (data, status) {
-                    new PNotify({
-                        title: status,
-                        text: data,
-                        type: 'error',
-                        hide: true,
-                        addclass: "stack-bottomright"
-                    });
-
-                });
-        }
-
-        function ReadUsers() {
             UserService.Read()
                 .then(function (response) {
                     vm.Users = response.data;
-                    UpdateUser();
                 })
                 .catch(function (data, status) {
                     new PNotify({
@@ -61,18 +59,15 @@
                         hide: true,
                         addclass: "stack-bottomright"
                     });
-
                 });
         }
 
-        function UpdateUser() {
-            angular.forEach(vm.Attendances, function (attendances) {
-                attendances.User = $filter('filter')(vm.Users, { UserId: attendances.UserId })[0];
-            });
+        function UpdateEmployee(user) {
+            user.Employee = $filter('filter')(vm.Employees, { EmployeeId: user.EmployeeId })[0];
         }
 
-        function Delete(attendanceId) {
-            AttendanceService.Delete(attendanceId)
+        function Delete(userId) {
+            UserService.Delete(userId)
                 .then(function (response) {
                     Read();
                 })
